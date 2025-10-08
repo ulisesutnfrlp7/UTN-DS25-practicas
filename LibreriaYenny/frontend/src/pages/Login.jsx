e51fda99a5ea8eccb7ccdaf92821fa5577dc2529
Ulises Mateo Bucchino <ulibucchino@gmail.com>
2025-10-07 23:31:07 -0300

    cambio login hardcodeado

--- LibreriaYenny/frontend/src/pages/Login.jsx
+++ LibreriaYenny/frontend/src/pages/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
   const onSubmit = async (data) => {
     setError("");
     try {
-      const res = await fetch("http://localhost:3000/api/auth/login", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
